5f1865cb7431694de651f9cda009eaf3b654a9d9
manzhizhen <manzhizhen@163.com>
2014-12-10 13:47:07 +0800

    新增枚举

--- SugarMQ/sugarmq-client/src/main/java/com/sugarmq/constant/MessageProperty.java
+++ SugarMQ/sugarmq-client/src/main/java/com/sugarmq/constant/MessageProperty.java
@@ -4,9 +4,31 @@
 package com.sugarmq.constant;
 
 /**
+ * 提供者用到的消息属性
  * @author manzhizhen
- *
+ * 
  */
 public enum MessageProperty {
-
+	DISABLE_MESSAGE_ID("_#_disableMessageId", false),
+	SESSION_ID("_#_sessionId", null);
+	
+	String key;
+	Object value;
+	private MessageProperty(String key, Object value) {
+		this.key = key;
+		this.value = value;
+	}
+	
+	public String getKey() {
+		return key;
+	}
+	
+	public Object getValue() {
+		return value;
+	}
+	
+	@Override
+	public String toString() {
+		return key;
+	}
 }
---
+++ SugarMQ/sugarmq-client/src/main/java/com/sugarmq/constant/MessageType.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.sugarmq.constant;
+
+/**
+ * 类说明：消息类型
+ *
+ * 类描述：
+ * @author manzhizhen
+ *
+ * 2014年12月10日
+ */
+public enum MessageType {
+	PRODUCER_MESSAGE("PRODUCER_MESSAGE"),	// 生产者发送的消息
+	CUSTOMER_ACKNOWLEDGE_MESSAGE("CUSTOMER_ACKNOWLEDGE_MESSAGE");	// 消费者应答消息
+	
+	String value;
+	private MessageType(String value) {
+		this.value = value;
+	}
+	
+	public String getValue() {
+		return value;
+	}
+	
+	@Override
+	public String toString() {
+		return value;
+	}
+}
